50401fe76a1fd3f2f803689ffe2505d82553415f
Dhawale, Dinkar <dinkar.dhawale@intel.com>
2019-08-21 23:14:04 -0700

    Add purge option for uninstall

--- app.go
+++ app.go
@@ -273,10 +273,10 @@ func (a *App) Run(args []string) error {
 	case "status":
 		return a.status()
 	case "uninstall":
-		var keepConfig bool
-		flag.CommandLine.BoolVar(&keepConfig, "keep-config", false, "keep config when uninstalling")
+		var purge bool
+		flag.CommandLine.BoolVar(&purge, "purge", false, "purge config when uninstalling")
 		flag.CommandLine.Parse(args[2:])
-		a.uninstall(keepConfig)
+		a.uninstall(purge)
 		os.Exit(0)
 	case "version":
 		fmt.Fprintf(a.consoleWriter(), "Auth Service %s-%s\n", version.Version, version.GitHash)
@@ -493,7 +493,7 @@ func (a *App) status() error {
 	return syscall.Exec(systemctl, []string{"systemctl", "status", "authservice"}, os.Environ())
 }
 
-func (a *App) uninstall(keepConfig bool) {
+func (a *App) uninstall(purge bool) {
 	fmt.Println("Uninstalling Auth Service")
 	removeService()
 
@@ -514,7 +514,7 @@ func (a *App) uninstall(keepConfig bool) {
 		log.WithError(err).Error("error removing ", a.execLinkPath())
 	}
 
-	if !keepConfig {
+	if purge {
 		fmt.Println("removing : ", a.configDir())
 		err = os.RemoveAll(a.configDir())
 		if err != nil {
