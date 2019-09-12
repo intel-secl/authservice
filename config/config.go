644d4e7ccec4ec23dc173f05f7d2bef8a0db449d
Dhawale, Dinkar <dinkar.dhawale@intel.com>
2019-09-12 10:01:00 -0700

    update config permission

--- config/config.go
+++ config/config.go
@@ -133,7 +133,7 @@ func (conf *Configuration) Save() error {
 		if os.IsNotExist(err) {
 			// error is that the config doesnt yet exist, create it
 			file, err = os.Create(conf.configFile)
-			os.Chmod(conf.configFile, 0660)
+			os.Chmod(conf.configFile, 0440)
 			if err != nil {
 				return err
 			}
