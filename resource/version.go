8b726e0977db78af5d9ee0b852ad5dc6c90d5633
Ghosh, Arijit <arijit.ghosh@intel.com>
2019-12-16 02:32:25 -0800

    Add STS header for version API

--- resource/version.go
+++ resource/version.go
@@ -19,6 +19,7 @@ func SetVersion(r *mux.Router) {
 func getVersion() http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		verStr := fmt.Sprintf("%s-%s", version.Version, version.GitHash)
+		w.Header().Add("Strict-Transport-Security", "max-age=63072000; includeSubDomains")
 		w.Write([]byte(verStr))
 	})
 }
